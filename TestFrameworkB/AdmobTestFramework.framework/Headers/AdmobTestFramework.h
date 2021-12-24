e9964cd691b33387c446f3b70e2a9e73e5d1075e
Wazma Ali <wazma.ali@consoliads.com>
2021-12-24 11:24:54 +0500

    version updated to 0.4.0

---
+++ TestFrameworkB/AdmobTestFramework.framework/Headers/AdmobTestFramework.h
@@ -0,0 +1,19 @@
+//
+//  AdmobTestFramework.h
+//  AdmobTestFramework
+//
+//  Created by OsamaNadeem on 12/24/21.
+//  Copyright © 2021 ConsoliAds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdmobTestFramework.
+FOUNDATION_EXPORT double AdmobTestFrameworkVersionNumber;
+
+//! Project version string for AdmobTestFramework.
+FOUNDATION_EXPORT const unsigned char AdmobTestFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdmobTestFramework/PublicHeader.h>
+
+
